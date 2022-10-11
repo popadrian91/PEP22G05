b864d1fa9312a8019375e32a9079e71689a9fa4a
adrian <popadrian91@gmail.com>
2022-10-11 19:28:32 +0300

    c

---
+++ modul2/part1.py
@@ -0,0 +1,28 @@
+# Output functions
+print('Hello World')
+
+#Input functions
+#a=input('Say Hello: ')
+#print('Hello ' + a)
+
+# Variabless
+my_name = 'Adi'
+print(my_name)
+#my_number = 1000000
+#my_number = 1_000_000
+my_number = 1000_000
+print(my_number)
+
+
+#type functions
+result = type(10)
+print (result)
+
+#return of print
+result=print('exemple')
+print(result)
+
+#return of input
+result=input('Say Hello: ')
+print(result)
+
