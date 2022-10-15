37fda0d1fd4368f69f47a956e67c93134b838f35
adrian <popadrian91@gmail.com>
2022-10-15 20:52:09 +0300

    Hommework V2

--- modul2/Homework/app4.py
+++ modul2/Homework/app4.py
@@ -1,5 +1,7 @@
 n=input('Introduceti un sir de caractere: ')
 print('Enter dupa fiecare caracter este:\n','\n'.join(n[i:i + 1] for i in range(0, len(n), 1))) #splituire after each character using /n
-print('Sirul introdus este: ', n,'\n' 'Sirul inversat este: ', n[::-1]) #print sir si sir inversat
+print('Enter dupa fiecare caracter este:\n','\n'.join(n[i:i + 3] for i in range(0, len(n), 3))) #splituire after each character using /n
+
+#print('Sirul introdus este: ', n,'\n' 'Sirul inversat este: ', n[::-1]) #print sir si sir inversat
 
 
