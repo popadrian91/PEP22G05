a956403f7a05980a14638a0a4c0db0c1d1b5b59e
adrian <popadrian91@gmail.com>
2022-10-25 21:13:05 +0300

    Part3

--- modul3/app7.py
+++ modul3/app7.py
@@ -2,5 +2,9 @@ lista = input("Introduceti lista de taskuri: ")
 lista_taskuri = lista.split(" ")
 print(lista_taskuri)
 
-lista_taskuri = list(dict.fromkeys(lista_taskuri))
-print(lista_taskuri)
+temp_list=[]
+for i in lista_taskuri:
+    if i not in temp_list:
+        temp_list.append(i)
+
+print(temp_list)
